f0ba59a1126e6bd28280a8cf8b168c20c0b07439
architraj99 <rajarchit99@gmail.com>
2026-04-28 20:09:46 +0530

    Added reset and focus/break mode switching
    Added reset and mode switching
    
    Timer now supports focus and break modes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let timeLeft = 25 * 60;
 let timerInterval = null;
+let currentMode = "Focus";
 
 function updateDisplay() {
     let minutes = Math.floor(timeLeft / 60);
@@ -17,7 +18,7 @@ function startTimer() {
         return;
     }
 
-    document.getElementById("status").innerText = "Running";
+    document.getElementById("status").innerText = currentMode + "_" + "Running";
 
     timerInterval = setInterval(function() {
         if (timeLeft > 0) {
@@ -39,4 +40,35 @@ function pauseTimer() {
     timerInterval = null;
 
     document.getElementById("status").innerText = "Paused";
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+
+    if (currentMode === "Focus") {
+    timeLeft = 25 * 60;
+    }
+    else {
+        timeLeft = 5 * 60;
+    }
+
+    updateDisplay();
+
+    document.getElementById("status").innerText = "Reset Done";
+
+}
+
+function setFocusMode() {
+    currentMode = "Focus";
+    document.getElementById("modeText").innerText = "Focus";
+
+    resetTimer();
+}
+
+function setBreakMode() {
+    currentMode = "Break";
+    document.getElementById("modeText").innerText = "Break";
+
+    resetTimer();
 }
