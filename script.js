6e641644c99dcdcfc6a8cb577fefdb322402d943
architraj99 <rajarchit99@gmail.com>
2026-04-28 23:56:23 +0530

    Java done and a few change in CSS
    Ready to ship

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let timeLeft = 25 * 60;
 let timerInterval = null;
 let currentMode = "Focus";
+let sessions= 0;
 
 function updateDisplay() {
     let minutes = Math.floor(timeLeft / 60);
     let seconds = timeLeft % 60;
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
 
     if (seconds < 10) {
         seconds = "0" + seconds;
@@ -13,12 +18,17 @@ function updateDisplay() {
     document.getElementById("timer").innerText = minutes + ":" + seconds;
 }
 
+function playSound(){
+    let audio = new Audio("https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg");
+    audio.play();
+}
+
 function startTimer() {
     if (timerInterval !== null) {
         return;
     }
 
-    document.getElementById("status").innerText = currentMode + "_" + "Running";
+    document.getElementById("status").innerText = currentMode + "Running";
 
     timerInterval = setInterval(function() {
         if (timeLeft > 0) {
@@ -30,7 +40,23 @@ function startTimer() {
             clearInterval(timerInterval);
             timerInterval = null;
 
-            document.getElementById("status").innerText = "Time's Up";
+            playSound();
+
+            if (currentMode === "Focus") {
+                sessions++;
+                document.getElementById("sessions").innerText = sessions;
+
+                document.getElementById("status").innerText = "Focus completed, Break started";
+
+                setBreakMode();
+                startTimer();
+            }
+
+            else {
+                document.getElementById("status").innerText = "Break's up, Let's Go Champ!!";
+
+                setFocusModel();
+            }
         }
     }, 1000);
 }
@@ -62,13 +88,17 @@ function resetTimer() {
 function setFocusMode() {
     currentMode = "Focus";
     document.getElementById("modeText").innerText = "Focus";
+    document.getElementById("modeDisplay").innerText = "Mode: Focus";
 
-    resetTimer();
+    timeLeft = 25 * 60;
+    updateDisplay();
 }
 
 function setBreakMode() {
     currentMode = "Break";
     document.getElementById("modeText").innerText = "Break";
+    document.getElementById("modeDisplay").innerText = "Mode: Break";
 
-    resetTimer();
+    timeLeft = 5 * 60;
+    updateDisplay();
 }
